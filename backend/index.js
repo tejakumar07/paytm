b29b5112fac7e40ef85bbc0f33a9738b92828b6b
tejakumar07 <tejakumarwarrior@gmail.com>
2026-05-07 10:18:09 +0530

    feat: update server configuration to use dynamic port and improve code structure

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const app = express();
 const cors = require("cors");
-const PORT = 3000;
 
+const app = express();
 
 const mainRouter = require("../backend/routes/index");
 
-
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use("/sapi/v1", mainRouter);
 
-app.listen(PORT)
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
